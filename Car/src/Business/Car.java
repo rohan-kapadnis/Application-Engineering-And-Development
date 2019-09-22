d24c45088dc6823b59451783ad686117fb71a4f9
Rohan Kapadnis <kapadnis.r@husky.neu.edu>
2019-09-22 15:59:27 -0400

    Assignment 1

---
+++ Car/src/Business/Car.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author rohan
+ */
+public class Car {
+    
+    private String name;
+    private String colour;
+    private String companyName;
+    private String year;
+    private String type;
+    private String safetyRating;
+    private String transmissionType;
+    private String keylessEntry;
+    private String wheelType;
+    private String autopilot;
+    private String sunroof;
+    private String numOfSeats;
+    private String topSpeed;
+    private String fuelType;
+    private String heatingSystem;
+    private String airbags;
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSafetyRating() {
+        return safetyRating;
+    }
+
+    public void setSafetyRating(String safetyRating) {
+        this.safetyRating = safetyRating;
+    }
+
+    public String getTransmissionType() {
+        return transmissionType;
+    }
+
+    public void setTransmissionType(String transmissionType) {
+        this.transmissionType = transmissionType;
+    }
+
+    public String getKeylessEntry() {
+        return keylessEntry;
+    }
+
+    public void setKeylessEntry(String keylessEntry) {
+        this.keylessEntry = keylessEntry;
+    }
+
+    public String getWheelType() {
+        return wheelType;
+    }
+
+    public void setWheelType(String wheelType) {
+        this.wheelType = wheelType;
+    }
+
+    public String getAutopilot() {
+        return autopilot;
+    }
+
+    public void setAutopilot(String autopilot) {
+        this.autopilot = autopilot;
+    }
+
+    public String getSunroof() {
+        return sunroof;
+    }
+
+    public void setSunroof(String sunroof) {
+        this.sunroof = sunroof;
+    }
+
+    public String getNumOfSeats() {
+        return numOfSeats;
+    }
+
+    public void setNumOfSeats(String numOfSeats) {
+        this.numOfSeats = numOfSeats;
+    }
+
+    public String getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(String topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getHeatingSystem() {
+        return heatingSystem;
+    }
+
+    public void setHeatingSystem(String heatingSystem) {
+        this.heatingSystem = heatingSystem;
+    }
+
+    public String getAirbags() {
+        return airbags;
+    }
+
+    public void setAirbags(String airbags) {
+        this.airbags = airbags;
+    }
+    
+    
+}
