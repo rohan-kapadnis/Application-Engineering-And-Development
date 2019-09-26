f43534fea30be1f401efd0c8c91ad624f700987a
Rohan Kapadnis <kapadnis.r@husky.neu.edu>
2019-09-26 12:01:18 -0400

    AED Lab2.5 first commit

---
+++ AEDLab2.5/src/Business/VitalSignHistory.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rohan
+ */
+public class VitalSignHistory{
+    private ArrayList<VitalSigns> vitalSignHistory;
+    private Iterable<VitalSigns> vitalSignList;
+    
+    public VitalSignHistory(){
+        
+        vitalSignHistory = new ArrayList<VitalSigns>();
+        
+    }
+
+
+
+    public ArrayList<VitalSigns> getVitalSignHistory() {
+        return vitalSignHistory;
+    }
+    
+
+    public void setVitalSignHistory(ArrayList<VitalSigns> vitalSignHistory) {
+        this.vitalSignHistory = vitalSignHistory;
+    }
+    
+    public VitalSigns addVitals(){
+        VitalSigns vs = new VitalSigns();
+        vitalSignHistory.add(vs);
+        return vs;
+    }
+    
+    public void deleteVitals(VitalSigns v){
+        vitalSignHistory.remove(v); 
+    }
+    
+    public List<VitalSigns> getAbnormalList(double maxbp, double minbp){
+        List<VitalSigns> abnList = new ArrayList<>();
+        for(VitalSigns vs: this.vitalSignHistory)
+        {
+            if(vs.getBloodPressure()> maxbp || vs.getBloodPressure()< minbp ){
+                abnList.add(vs);
+            }
+        }
+        return abnList;
+    }
+    
+}
